6079545e5dc8e6247eba5b0258d80513868c840a
ritaurbani <ritaurbani@gmail.com>
2025-03-23 22:54:23 +0100

    feat: completed

---
+++ Bonus1.js
@@ -0,0 +1,49 @@
+
+// Bonus 1 GESTISCI ERRORI TRA UNA RICERCA E UN ALTRA 
+//ci aiuta a capire dov e l errore e di che tipo e
+// ex: id (558493422) ex: link false per stabilire a quale fetch si riferisce
+// Attualmente, se la prima richiesta non trova una ricetta, 
+// la seconda richiesta potrebbe comunque essere eseguita causando errori a cascata.
+// Modifica getChefBirthday(id) per intercettare eventuali errori prima di fare la seconda richiesta.
+
+async function getChefBirthday(id) {
+    
+    let recipes
+    try{
+        const response = await fetch(`https://dummyjson.com/recipes/${6798888}`)//dato resolve al fetch
+        recipes = await response.json();
+        console.log(recipes)//recipes ha cmq al suo interno qualcosa: oggetto con messaggio che contiene l errore
+    }catch(error){//se c e errore > lo stampo in console e throw errore generico catturato da try catch sotto
+        throw new Error(`Non trova la ricetta con id ${id}`)
+    }
+    if(recipes.message){//controlliamo se abbiamo ottento recipes - se tutto ok andiamo a fetch successivo
+        throw new Error(recipes.message)//stampo messaggio direttamente
+    }
+
+    let chef
+    try{
+    const user = await fetch(`https://dummyjson.com/users/${recipes.userId}`)//se metto url falso fetch cannot obtain data
+    chef = await user.json()
+    }catch(error){
+        console.error(error)
+        throw new Error(`chef con id ${id} non trovato`)
+    }
+
+    if(!chef){//chef non e stato trovato non ha senso il return
+        throw new Error(`chef con id ${id}non trovato`)
+    }// return {...recipes, chef} //ritorna promise che risolve quel valore
+    const newdate = dayjs(chef.birthDate).format('DD/MM/YYYY')
+    return newdate
+}
+
+(async () => {//funzione anonima asincrona eseguita subito 1.creiamo contenuto 2.wrappiamo in try-catch
+    try {
+        const birthday = await getChefBirthday(1)
+        console.log("Data di nascita dello chef:", birthday)
+    } catch (error) {
+        console.error("Errore:", error.message)
+    }
+    console.log("Fine!")//lo vediamo perche abbiamo gestito bene
+})()
+
+// console.log(dayjs) //ritorna in console una funzione per lavorare ocn le date
---
+++ getChefBirthday.js
@@ -0,0 +1,57 @@
+// TRACCIA
+
+// In questo esercizio, utilizzerai async / await per creare la funzione getChefBirthday(id).
+// Questa funzione accetta un id di una ricetta e deve:
+
+// Recuperare la ricetta da https://dummyjson.com/recipes/{id}
+// Estrarre la proprietà userId dalla ricetta
+// Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
+// Restituire la data di nascita dello chef
+
+// Note del docente
+
+// Scrivi la funzione getChefBirthday(id), che deve:
+
+// Essere asincrona(async).
+// Utilizzare await per chiamare le API.
+// Restituire una Promise con la data di nascita dello chef.
+// Gestire gli errori con try/catch
+
+// Esempio di utilizzo
+
+// getChefBirthday(1)
+//     .then(birthday => console.log("Data di nascita dello chef:", birthday))
+//     .catch(error => console.error("Errore:", error.message));
+
+// Esempio di output atteso:
+
+// Data di nascita dello chef: 1990-06 - 15
+
+//ESECUZIONE
+
+async function getChefBirthday(id){
+    const response = await fetch(`https://dummyjson.com/recipes/${id}`)//dato resolve al fetch
+    const recipes = await response.json();
+    console.log(recipes)//vedo a ceh punto sono
+    const user = await fetch(`https://dummyjsonfalso.com/users/${recipes.userId}`)//se metto url falso fetch cannot obtain data-pero errore fialed to fetch non ci dice a quale fetch si riferisce
+    const chef = await user.json()
+    console.log(chef)
+    // return {...recipes, chef} //ritorna promise che risolve quel valore
+    return chef.birthDate
+}
+
+
+
+// getChefBirthday(1)
+// .then(birthday => console.log("Data di nascita dello chef:", birthday))
+// .catch(error => console.error("Errore:", error.message));
+
+(async() => {//funzione anonima asincrona eseguita subito 1.creiamo contenuto 2.wrappiamo in try-catch
+try {
+    const birthday = await getChefBirthday(1)
+console.log("Data di nascita dello chef:", birthday)
+}catch(error){
+    console.error("Errore:", error.message)
+}
+console.log("Fine!")//lo vediamo perche abbiamo gestito bene
+})()
